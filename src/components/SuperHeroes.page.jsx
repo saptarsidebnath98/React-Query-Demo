dad73eaeae5ed3893f40ada531a2cb2215f59f09
saptarsidebnath98 <saptarsidebnath666666@gmail.com>
2025-09-14 02:45:33 +0530

    traditional fetch call with axios

--- src/components/SuperHeroes.page.jsx
+++ src/components/SuperHeroes.page.jsx
@@ -1,7 +1,29 @@
+import axios from "axios";
+import { useEffect, useState } from "react"
+
 const SuperHeroes = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:4000/superheros').then((res) => {
+      setData(res.data);
+      setIsLoading(false);
+    })
+  }, [])
+
+  if(isLoading) return <h2>Loading...</h2>
+
+  console.log(data)
+
   return (
     <div>
-      SuperHeroes Page
+      <h2 className="font-bold text-2xl mb-2">Super Heroes Page</h2>
+      {data.map((hero) => (
+        <div key={hero.id}>
+          {hero.name}
+        </div>
+      ))}
     </div>
   )
 }
